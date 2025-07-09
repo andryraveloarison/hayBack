f64b5b18391c50ab8d5302f8df6f82c18c62a2de
Andry <andryraveloarison01@gmail.com>
2025-07-09 15:29:00 +0300

    ajout morgan

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,14 @@ import subjectRoutes from "./presentation/http/SubjectController"
 import chapterRoutes from "./presentation/http/ChapterController"
 import courseRoutes from "./presentation/http/CourseController"
 import iaRoutes from "./presentation/http/IAController"
+import morgan from "morgan";
 
 import cors from "cors";
 
 dotenv.config();
 const app = express();
 
+app.use(morgan('dev'))
 app.use(cors({
     origin: "*", // Remplacez par l'URL de votre frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Méthodes autorisées
@@ -33,14 +35,13 @@ app.get("/users", (req, res) => {
     res.send("users");
   });
 
-  
+
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/levels", levelRoutes);
 app.use("/api/subjects", subjectRoutes);
 app.use("/api/chapters", chapterRoutes);
 app.use("/api/courses", courseRoutes);
-
 app.use("/api/ia", iaRoutes);
 
 const PORT = process.env.PORT || 8000;
